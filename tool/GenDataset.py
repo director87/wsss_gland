b253595a278c79f3de5b7f57f6d7a7918f5c2112
director87 <director87@foxmail.com>
2024-12-11 21:04:06 +0800

    Update GenDataset.py

--- tool/GenDataset.py
+++ tool/GenDataset.py
@@ -57,24 +57,11 @@ class Stage1_TrainDataset(Dataset):
             for f in filename:
                 image_path = os.path.join(root, f)
                 fname = f[:-4]
-                ##  Extract the image-level label from the filename
-                ##  LUAD-HistoSeg   : 'Image-name-of-BCSS'+'+index'+'[abcd]'.png
-                ##  BCSS-WSSS       : 'patient_ID'+'_x-axis'+'_y-axis'+'[a b c d]'.png
                 label_str = fname.split(']')[0].split('[')[-1]
-                if self.dataset == 'luad':
-                    image_label = torch.Tensor([int(label_str[0]),int(label_str[2]),int(label_str[4]),int(label_str[6])])
-                elif self.dataset == 'bcss':
-                    image_label = torch.Tensor([int(label_str[0]),int(label_str[1]),int(label_str[2]),int(label_str[3])])
-                elif self.dataset == 'wsss':
-                    image_label = torch.Tensor([int(label_str[0]), int(label_str[3]), int(label_str[6])])
                 elif self.dataset == 'ring':
                     image_label = torch.Tensor([int(label_str[0]), int(label_str[1])])
                 elif self.dataset == 'glas':
                     image_label = torch.Tensor([int(label_str[3]), int(label_str[0])])
-                elif self.dataset == 'crag':
-                    image_label = torch.Tensor([int(label_str[0]), int(label_str[1])])
-                elif self.dataset == 'gcss':
-                    image_label = torch.Tensor([int(label_str[0]), int(label_str[1]), int(label_str[2]), int(label_str[3]), int(label_str[4]), int(label_str[5])])
                 path_label.append((image_path, image_label))
         return path_label
 
@@ -87,24 +74,15 @@ class Stage2_Dataset(Dataset):
 
         if self.split   == "train":
             self._image_dir     = os.path.join(self._base_dir, 'train/')
-            # self._image_dir     = os.path.join(self._base_dir, 'train/')
-            # self._image_dir     = os.path.join(self._base_dir, 'train_1p/')
-            self._cat_dir       = os.path.join(self._base_dir, 'train_PM/PM_res38d_cambn7/')
-            self._cat_dir_a     = os.path.join(self._base_dir, 'train_PM/PM_res38d_gpp_pam_camb5_2')
-            self._cat_dir_b     = os.path.join(self._base_dir, 'train_PM/PM_res38d_gpp_pam_camb4_5')
-            # self._cat_dir = os.path.join(self._base_dir, 'train_PM/PM_FSbn7/')
-            # self._cat_dir_a = os.path.join(self._base_dir, 'train_PM/PM_FSb5_2')
-            # self._cat_dir_b = os.path.join(self._base_dir, 'train_PM/PM_FSb4_5')
+            self._cat_dir       = os.path.join(self._base_dir, 'train_PM/PM_res38d_refinedbn7/')
+            self._cat_dir_a     = os.path.join(self._base_dir, 'train_PM/PM_res38d_refinedb5_2')
+            self._cat_dir_b     = os.path.join(self._base_dir, 'train_PM/PM_res38d_refinedb4_5')
         elif self.split == 'val':
-            # self._image_dir = os.path.join(self._base_dir, 'val/img/')
-            # self._cat_dir   = os.path.join(self._base_dir, 'val/mask/')
-            self._image_dir = os.path.join(self._base_dir, 'val_448/img/')
-            self._cat_dir = os.path.join(self._base_dir, 'val_448/mask/')
+            self._image_dir = os.path.join(self._base_dir, 'val/img/')
+            self._cat_dir   = os.path.join(self._base_dir, 'val/mask/')
         elif self.split == 'test':
-            # self._image_dir = os.path.join(self._base_dir, 'test/img/')
-            # self._cat_dir   = os.path.join(self._base_dir, 'test/mask/')
-            self._image_dir = os.path.join(self._base_dir, 'test_448/img/')
-            self._cat_dir = os.path.join(self._base_dir, 'test_448/mask/')
+            self._image_dir = os.path.join(self._base_dir, 'test/img/')
+            self._cat_dir   = os.path.join(self._base_dir, 'test/mask/')
         self.args = args
         self.filenames = [os.path.splitext(file)[0] for file in os.listdir(self._image_dir) if not file.startswith('.')]
         self.images = [os.path.join(self._image_dir, fn + '.png') for fn in self.filenames]
@@ -168,82 +146,6 @@ class Stage2_Dataset(Dataset):
     def __str__(self):
         return None
 
-
-class Stage2_Dataset_single(Dataset):
-    def __init__(self, args, base_dir, split):
-
-        super().__init__()
-        self._base_dir = base_dir
-        self.split = split
-
-        if self.split   == "train":
-            self._image_dir     = os.path.join(self._base_dir, 'train/')
-            self._cat_dir       = os.path.join(self._base_dir, 'train_PM/PM_wr38/')
-        elif self.split == 'val':
-            self._image_dir = os.path.join(self._base_dir, 'val/img/')
-            self._cat_dir   = os.path.join(self._base_dir, 'val/mask/')
-        elif self.split == 'test':
-            self._image_dir = os.path.join(self._base_dir, 'test/img/')
-            self._cat_dir   = os.path.join(self._base_dir, 'test/mask/')
-        self.args = args
-        self.filenames = [os.path.splitext(file)[0] for file in os.listdir(self._image_dir) if not file.startswith('.')]
-        self.images = [os.path.join(self._image_dir, fn + '.png') for fn in self.filenames]
-        self.categories = [os.path.join(self._cat_dir, fn + '.png') for fn in self.filenames]
-        # if self.split == "train":
-        #     self.categories_a = [os.path.join(self._cat_dir_a, fn + '.png') for fn in self.filenames]
-        #     self.categories_b = [os.path.join(self._cat_dir_b, fn + '.png') for fn in self.filenames]
-        assert (len(self.images) == len(self.categories))
-        print('Number of images in {}: {:d}'.format(split, len(self.images)))
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, index):
-        if self.split == "train":
-            _img, _target = self._make_img_gt_point_pair(index)
-            sample = {'image': _img, 'label': _target}
-        elif (self.split == 'val') or (self.split == 'test'):
-            _img, _target, = self._make_img_gt_point_pair(index)
-            sample = {'image': _img, 'label': _target}
-            image_dir = self.images[index]
-        if self.split == "train":
-            return self.transform_tr_ab(sample)
-        elif (self.split == 'val') or (self.split == 'test'):
-            return self.transform_val(sample), image_dir
-    def _make_img_gt_point_pair(self, index):
-        if self.split == "train":
-            _img = Image.open(self.images[index]).convert('RGB')
-            _target = Image.open(self.categories[index])
-            return _img,_target
-        elif (self.split == 'val') or (self.split == 'test'):
-            _img = Image.open(self.images[index]).convert('RGB')
-            _target = Image.open(self.categories[index])
-            return _img,_target
-
-    def transform_tr(self, sample):
-        composed_transforms = transforms.Compose([
-            tr.RandomHorizontalFlip(),
-            tr.RandomGaussianBlur(),
-            tr.Normalize(),
-            tr.ToTensor()])
-        return composed_transforms(sample)
-
-    def transform_tr_ab(self, sample):
-        composed_transforms = transforms.Compose([
-            tr.RandomHorizontalFlip_ab(),
-            tr.RandomGaussianBlur_ab(),
-            tr.Normalize_ab(),
-            tr.ToTensor_ab()])
-        return composed_transforms(sample)
-
-    def transform_val(self, sample):
-        composed_transforms = transforms.Compose([
-            tr.Normalize(),
-            tr.ToTensor()])
-        return composed_transforms(sample)
-
-    def __str__(self):
-        return None
 
 
 def make_data_loader(args, **kwargs):
