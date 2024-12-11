53078e87d5825d1618c51e50407d52235c3a38ec
director87 <director87@foxmail.com>
2024-12-11 20:13:00 +0800

    Update 2_generate_PM.py

--- 2_generate_PM.py
+++ 2_generate_PM.py
@@ -9,36 +9,16 @@ from tool.infer_fun import create_pseudo_mask
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--weights", default='E:/code/WSSS-Tissue-main/WSSS-Tissue-main/checkpoints/stage1_checkpoint_trained_on_crag_res38d_cam.pth', type=str)
-    parser.add_argument("--network", default="E:/code/WSSS-Tissue-main/WSSS-Tissue-main/network.resnet38_cls", type=str)
-    parser.add_argument("--dataroot", default="E:/code/WSSS-Tissue-main/WSSS-Tissue-main/datasets/CRAG", type=str)
-    parser.add_argument("--dataset", default="crag", type=str)
-    parser.add_argument("--num_workers", default=1, type=int)
+    parser.add_argument("--weights", default='checkpoints/stage1_checkpoint_trained_on_ring_res38d.pth', type=str)
+    parser.add_argument("--network", default="network.resnet38_cls", type=str)
+    parser.add_argument("--dataroot", default="datasets/RINGS", type=str)
+    parser.add_argument("--dataset", default="ring", type=str)
+    parser.add_argument("--num_workers", default=10, type=int)
     parser.add_argument("--n_class", default=2, type=int)
 
     args = parser.parse_args()
     print(args)
-    if args.dataset == 'luad':
-        palette = [0]*15
-        palette[0:3] = [205,51,51]
-        palette[3:6] = [0,255,0]
-        palette[6:9] = [65,105,225]
-        palette[9:12] = [255,165,0]
-        palette[12:15] = [255, 255, 255]
-    elif args.dataset == 'bcss':
-        palette = [0]*15
-        palette[0:3] = [255, 0, 0]
-        palette[3:6] = [0,255,0]
-        palette[6:9] = [0,0,255]
-        palette[9:12] = [153, 0, 255]
-        palette[12:15] = [255, 255, 255]
-    elif args.dataset == 'wsss':
-        palette = [0]*12
-        palette[0:3] = [0, 64, 128]
-        palette[3:6] = [64, 128, 0]
-        palette[6:9] = [243, 125, 0]
-        palette[9:12] = [255, 255, 255]
-    elif args.dataset == 'ring':
+    if args.dataset == 'ring':
         palette = [0] * 6
         palette[0:3] = [68, 0, 83]
         palette[3:6] = [254, 230, 35]
@@ -46,20 +26,6 @@ if __name__ == '__main__':
         palette = [0] * 6
         palette[0:3] = [0, 64, 128]
         palette[3:6] = [64, 128, 0]
-    elif args.dataset == 'crag':
-        palette = [0] * 6
-        palette[0:3] = [255, 20, 20]
-        palette[3:6] = [255, 255, 0]
-        # palette[0:3] = [255, 255, 255]
-        # palette[3:6] = [0, 0, 0]
-    elif args.dataset == 'gcss':
-        palette = [0]*18
-        palette[0:3] = [255, 255, 255]
-        palette[3:6] = [255,0,0]
-        palette[6:9] = [64,128,0]
-        palette[9:12] = [0, 255, 255]
-        palette[12:15] = [255, 165, 0]
-        palette[15:18] = [68,0,83]
     PMpath = os.path.join(args.dataroot,'train_PM')
     if not os.path.exists(PMpath):
         os.mkdir(PMpath)
@@ -68,20 +34,20 @@ if __name__ == '__main__':
     model.eval()
     model.cuda()
     ##
-    # fm = 'b4_5'
-    # savepath = os.path.join(PMpath,'PM_'+'res38d_gpp_pam_cam'+fm)
-    # if not os.path.exists(savepath):
-    #     os.mkdir(savepath)
-    # create_pseudo_mask(model, args.dataroot, fm, savepath, args.n_class, palette, args.dataset)
-    # ##
-    # fm = 'b5_2'
-    # savepath = os.path.join(PMpath,'PM_'+'res38d_gpp_pam_cam'+fm)
-    # if not os.path.exists(savepath):
-    #     os.mkdir(savepath)
-    # create_pseudo_mask(model, args.dataroot, fm, savepath, args.n_class, palette, args.dataset)
+    fm = 'b4_5'
+    savepath = os.path.join(PMpath,'PM_'+'res38d_refined'+fm)
+    if not os.path.exists(savepath):
+        os.mkdir(savepath)
+    create_pseudo_mask(model, args.dataroot, fm, savepath, args.n_class, palette, args.dataset)
+    ##
+    fm = 'b5_2'
+    savepath = os.path.join(PMpath,'PM_'+'res38d_refined'+fm)
+    if not os.path.exists(savepath):
+        os.mkdir(savepath)
+    create_pseudo_mask(model, args.dataroot, fm, savepath, args.n_class, palette, args.dataset)
     ##
     fm = 'bn7'
-    savepath = os.path.join(PMpath,'PM_'+'res38d_cam'+fm)
+    savepath = os.path.join(PMpath,'PM_'+'res38d_refined'+fm)
     if not os.path.exists(savepath):
         os.mkdir(savepath)
     create_pseudo_mask(model, args.dataroot, fm, savepath, args.n_class, palette, args.dataset)
